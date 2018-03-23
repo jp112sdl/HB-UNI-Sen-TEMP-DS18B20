792cac17701f408346a715287f9a3acfb1aac403
jp112sdl <jp112sdl@icloud.com>
2018-03-23 09:56:11 +0100

    making everything more reusable added uninstall routine to remove added values in webui.js und DEVDB.tcl

--- Addon/build.sh
+++ Addon/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-rm HB-UNI-Sen-TEMP-DS18B20-addon.tgz
+DEVICE="HB-UNI-Sen-TEMP-DS18B20" 
+rm $DEVICE-addon.tgz
 find . -name ".DS_Store" -exec rm -rf {} \;
-cd HB-UNI-Sen-TEMP-DS18B20-addon-src
+cd $DEVICE-addon-src
 chmod +x update_script
 chmod +x addon/install*
+chmod +x addon/uninstall*
 chmod +x addon/update-check.cgi
 chmod +x rc.d/*
-tar -zcvf ../HB-UNI-Sen-TEMP-DS18B20-addon.tgz *
+tar -zcvf ../$DEVICE-addon.tgz *
 cd ..
